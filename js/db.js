d86503977ddabc190531846f0a49230ea91363e6
sarganar <sarganar@gmail.com>
2021-03-05 10:02:26 -0300

    !

--- js/db.js
+++ js/db.js
@@ -1,25 +1,27 @@
+export async function fectchDB() {
+  const urlBase = "https://sarganar.github.io/appDLR/db/";
+  const urlProducts = "products.json";
+  const urlDiscounts = "discounts.json";
+  const urlZeroStock = "zero_stock.json";
+  const urlContagramDB = "contagramDB.json";
 
+  try {
+    let response = await fetch(`${urlBase}${urlProducts}`);
+    const products = await response.json();
+    console.log("products:", products);
 
-export async function fectchDB(){
-    const urlBase = "https://sarganar.github.io/appDLR/db/";
-    const urlProducts='products.json';
-    const urlDiscounts='discounts.json';
-    const urlZeroStock='zero_stock.json';
-    const urlContagramDB='contagramDB.json';
-    
+    response = await fetch(`${urlBase}${urlDiscounts}`);
+    const discounts = await response.json();
+    console.log("discounts:", discounts);
 
-    try {
-      let response = await fetch(`${urlBase}${urlProducts}`);
-      const products = await response.json();
-      console.log(products);
+    response = await fetch(`${urlBase}${urlZeroStock}`);
+    const zeroStock = await response.json();
+    console.log("zeroStock:", zeroStock);
 
-      response = await fetch(`${urlBase}${urlDiscounts}`);
-      const discounts = await response.json();
-      console.log(discounts);
-
-
-
-    } catch (error) {
-      console.log(error);
-    }    
+    response = await fetch(`${urlBase}${urlContagramDB}`);
+    const contagramDB = await response.json();
+    console.log("contagramDB", contagramDB);
+  } catch (error) {
+    console.log('error:',error);
+  }
 }
